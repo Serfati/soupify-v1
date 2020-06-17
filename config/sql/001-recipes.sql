13105d01d58af31374ce132058210ad03c7dba2b
Avihai Serfati <avihaipp@gmail.com>
2020-06-17 23:00:27 +0300

    :camera: cloudinary image upload

--- config/sql/001-recipes.sql
+++ config/sql/001-recipes.sql
@@ -1,19 +1,22 @@
-numcreate
-table public.recipes
+create table public.recipes
 (
-    id serial not null
-    constraint recipes_pk primary key,
-    title varchar (100) not null,
-    ready_in_minutes integer,
-    aggregate_likes integer,
-    serving integer,
-    vegetarian boolean,
-    vegan boolean,
-    gluten_free boolean,
-    image text,
-    instructions text,
+    id                   serial       not null
+        constraint recipes_pk primary key,
+    title                varchar(100) not null,
+    ready_in_minutes     integer,
+    aggregate_likes      integer,
+    serving              integer,
+    vegetarian           boolean,
+    vegan                boolean,
+    gluten_free          boolean,
+    image                text,
+    instructions         text,
     extended_ingredients text
 );
 
 alter table public.recipes
     owner to soupify;
+
+ALTER SEQUENCE recipes_id_seq RESTART WITH 100;
+UPDATE recipes
+SET id=nextval('recipes_id_seq');
--- controllers/files.js
+++ controllers/files.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
 const HttpStatus = require('http-status-codes');
-const {v4: uuidv4} = require("uuid");
-const Formidable = require("formidable");
 const cloudinary = require("cloudinary");
 const path = require("path");
-const FileModel = require("../models/FileModel");
-const url = require("url");
 const ErrorMessageModel = require("../models/ErrorMessageModel");
 
-router.post("/", passport.authenticate("jwt", {session: false}), uploadImg);
 
 cloudinary.config({
     cloud_name: process.env.cloudinary_name,
@@ -18,54 +12,37 @@ cloudinary.config({
     api_secret: process.env.cloudinary_secret
 });
 
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const multerUploads = multer({storage}).single('image');
+const DatauriParser = require('datauri/parser');
+const parser = new DatauriParser();
+const dataUri = req => parser.format(path.extname(req.files.image.name).toString(), req.files.image.data);
+//
+
+router.post("/", multerUploads, uploadImg);
 
 //front-end
 async function uploadImg(req, res) {
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
-        return res
-            .status(HttpStatus.BAD_REQUEST)
-            .send(new ErrorMessageModel("No files were uploaded."));
-    }
     try {
-        const form = new Formidable();
-        form.parse(req, (err, fields, files) => {
-            cloudinary.uploader.upload(files.upload.path, (result) => {
-                if (result.public_id) {
-                    console.log(result.secure_url);
-                    let file = req.files.file;
-                    let realFileName = file.name;
-                    const extension = path.extname(realFileName);
-                    let randomFileName = `${uuidv4()}.${extension}`;
+        if (req.files.image) {
+            const file = dataUri(req).content;
 
-                    let fileInfo = new FileModel(
-                        undefined,
-                        realFileName,
-                        extension,
-                        randomFileName
-                    );
+            cloudinary.uploader.upload(file, (result) => {
+                if (result.public_id) {
                     let returnedValue = {
-                        id: fileInfo.id,
-                        name: fileInfo.name,
-                        type: fileInfo.type,
+                        url: result.secure_url
                     };
-                    res.status(HttpStatus.CREATED).json(result, returnedValue);
+                    res.status(HttpStatus.CREATED).json(returnedValue);
                 } else
-                    return res.status(HttpStatus.BAD_REQUEST).send(new ErrorMessageModel("No files were uploaded." + err.message));
+                    return res.status(HttpStatus.BAD_REQUEST).send(new ErrorMessageModel("No files were uploaded."));
             });
-        });
+        }
     } catch (e) {
         return res
             .status(HttpStatus.INTERNAL_SERVER_ERROR)
             .send({message: e.message});
     }
-}
-
-function fullUrl(req) {
-    return url.format({
-        protocol: req.protocol,
-        host: req.get("host"),
-        pathname: req.originalUrl,
-    });
 }
 
 module.exports = router;
